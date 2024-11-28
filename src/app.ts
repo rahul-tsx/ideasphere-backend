2ec8b63ab1b6db80b278e66789da4c56ba2903ff
Rahul Nair <rahulsunilnair1074@gmail.com>
2024-11-28 22:33:19 +0530

    fix:cors options issue

--- src/app.ts
+++ src/app.ts
@@ -25,6 +25,7 @@ const corsOptions: CorsOptions = {
 	credentials: true,
 };
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(cookieParser());
