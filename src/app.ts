982b76a60c671c6828e7aa60e1f50006b382a5bc
Rahul Nair <rahulsunilnair1074@gmail.com>
2024-11-28 22:49:07 +0530

    fix:cors error 3

--- src/app.ts
+++ src/app.ts
@@ -26,10 +26,16 @@ const corsOptions: CorsOptions = {
 	credentials: true,
 };
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', allowedOrigins[0]);
+
+	next();
+});
 //routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/content', contentRouter);
